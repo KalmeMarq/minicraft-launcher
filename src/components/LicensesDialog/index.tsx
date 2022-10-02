df94fc20ec70c48cd533b75ef68ade616a576493
KalmeMarq <marq520filip56.fm@gmail.com>
2022-10-02 03:03:46 +0100

    fixed licenses text undefined

--- src/components/LicensesDialog/index.tsx
+++ src/components/LicensesDialog/index.tsx
@@ -16,7 +16,7 @@ const LicensesDialog: React.FC<{ isOpen?: boolean; onClose?: () => void }> = ({ 
             {lc.author && <p className="license-author">{lc.author}</p>}
             <span className="license-version">{lc.version}</span>
             <p className="license">{lc.license}</p>
-            {lc.licenseText.length > 0 && <div className="license-content">{lc.licenseText}</div>}
+            {lc.licenseText && lc.licenseText.length > 0 && <div className="license-content">{lc.licenseText}</div>}
           </div>
         ))}
       </div>
